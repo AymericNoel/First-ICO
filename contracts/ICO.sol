6bddf3583f0ad0cdca5a14e50a45463ff4357047
AymericNoel <aymeric.noel@edu.devinci.fr>
2020-03-02 00:23:58 +0100

    My first ICO

---
+++ contracts/ICO.sol
@@ -0,0 +1,69 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+import "./ERC20.sol";
+
+contract ICO is ERC20{
+    using SafeMath for uint256;
+
+    struct user{
+        address eth_adress;
+        uint16 rank;
+    }
+    address private _owner;
+    user[] private _whiteList;
+    event addCustomer (bool added);
+
+    constructor() public{
+        _owner = msg.sender;
+    }
+
+    modifier OnlyOwner() {
+        require (msg.sender == _owner, "Only owner can access");
+        _;
+    }
+    function AddToWhiteList(address _newMember) public OnlyOwner{
+        if(_whiteList.length < 10){
+            _whiteList.push(user(_newMember,4));
+            emit addCustomer(true);
+        }
+        else{
+            if(_whiteList.length < 20){
+            _whiteList.push(user(_newMember,3));
+            emit addCustomer(true);
+            }
+            else{
+                if(_whiteList.length < 30){
+                _whiteList.push(user(_newMember,2));
+                emit addCustomer(true);
+                }
+                else{
+                    _whiteList.push(user(_newMember,1));
+                    emit addCustomer(true);
+                }
+            }
+        }
+    }
+    function ExistInList(address myAdress) public view returns(bool,uint256){
+        bool exist = false;
+        uint256 id = 0;
+        for (uint i = 0 ; i < _whiteList.length ; i++){
+            if(_whiteList[i].eth_adress == myAdress){
+                exist = true;
+                id = i;
+            }
+        }
+        return (exist,id);
+    }
+    function donateToken(address _receiver, uint256 _amount) public OnlyOwner(){
+        _mint(_receiver, _amount);
+    }
+
+    function _receiveToken() public payable {
+        bool exist;
+        (exist,) = ExistInList(msg.sender);
+        require(exist,"Must be in list of donaters");
+        uint256 id;
+        (,id) = ExistInList(msg.sender);
+        _mint(msg.sender, msg.value * _whiteList[id].rank);
+    }
+}
---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,10 @@
+const Migrations = artifacts.require("Migrations");
+const ERC20= artifacts.require("ERC20.sol");
+const ICO =artifacts.require("ICO.sol");
+
+
+module.exports = function(deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(ERC20);
+  deployer.deploy(ICO);
+};
